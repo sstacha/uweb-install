2efb6c5903cbe1724b63a3243cdb1169b1daed28
sstacha <sstacha@gmail.com>
2017-09-25 10:18:26 -0500

    Making command more specific

--- uweb_files/management/commands/secret_key.py
+++ uweb_files/management/commands/secret_key.py
@@ -2,7 +2,19 @@ from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
 
 class Command(BaseCommand):
-    help = 'Administrative Commands'
+    help = "\n"
+    help += "usage: ./manage.py secret_key [option]\n"
+    help += "--------------------------------------\n"
+    help += "example: ./manage.py admin generate\n"
+    help += "example: ./manage.py secret_key get\n"
+    help += "example: ./manage.py admin set [key]\n"
+    help += "\n"
+    help += "options\n"
+    help += "--------\n"
+    help += "generate - generates a new secret key and prints it to the screen\n"
+    help += "get - prints the current .secret_key file value\n"
+    help += "set - sets the .secret_key file value and prints it to the screen\n"
+    help += "\n"
 
     def add_arguments(self, parser):
         parser.add_argument('option', nargs='+', type=str)
@@ -29,18 +41,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # self.stdout.write(self.style.SUCCESS("subcommand: " + str(options['subcommand'])))
         # self.stdout.write(self.style.SUCCESS("options: " + str(options)))
-        if "generate_secret" in options['option']:
+        if "generate" in options['option']:
             self.stdout.write(self.style.SUCCESS('%s' % self.generate_secret()))
+        elif "get" in options['option']:
+            self.stdout.write(self.style.SUCCESS('%s' % self.get_secret()))
+        elif "set" in options['option']:
+            self.stdout.write(self.style.SUCCESS('%s' % self.set_secret()))
         else:
-            self.stdout.write(self.style.SUCCESS(" "))
-            self.stdout.write(self.style.SUCCESS("usage: ./manage.py admin [option]"))
-            self.stdout.write(self.style.SUCCESS("example: ./manage.py admin get_secret"))
-            self.stdout.write(self.style.SUCCESS("example: ./manage.py admin set_secret [key]"))
-            self.stdout.write(self.style.SUCCESS("example: ./manage.py admin generate_secret"))
-            self.stdout.write(self.style.SUCCESS(" "))
-            self.stdout.write(self.style.SUCCESS("options "))
-            self.stdout.write(self.style.SUCCESS("--------"))
-            self.stdout.write(self.style.SUCCESS("generate_secret - generates a new secret key and prints it to the screen"))
-            self.stdout.write(self.style.SUCCESS("get_secret - prints the current .secret_key value"))
-            self.stdout.write(self.style.SUCCESS("set_secret - sets the .secret_key value and prints it to the screen"))
-            self.stdout.write(self.style.SUCCESS(" "))
+            self.stdout.write(self.style.SUCCESS(self.help))
