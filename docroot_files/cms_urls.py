c80163304221b673b0d3c65473654146f6f88245
sstacha <sstacha@gmail.com>
2017-10-20 16:12:46 -0500

    Adding include definition so works after copy

--- docroot_files/cms_urls.py
+++ docroot_files/cms_urls.py
@@ -1,6 +1,7 @@
 # ITEMS BETWEEN THESE HEADINGS WILL BE UPDATED
 # ------------------------ UWEB URLS ------------------------------------
 # add our different urls for the cms to work
+from django.conf.urls import include
 urlpatterns += [
     url(r'^_cms/', include('cms.urls'))
 ]
