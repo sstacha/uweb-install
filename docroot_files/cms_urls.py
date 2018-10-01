add2af449a6dbc80090fde177ea75d6d40d8ad1d
sstacha <sstacha@gmail.com>
2018-10-01 13:35:19 -0500

    Adding include since the one built by installer now does path only

--- docroot_files/cms_urls.py
+++ docroot_files/cms_urls.py
@@ -1,7 +1,9 @@
 # ITEMS BETWEEN THESE HEADINGS WILL BE UPDATED
 # ------------------------ UWEB URLS ------------------------------------
 # add our different urls for the cms to work
+from django.conf.urls import url
 from django.conf.urls import include
+
 urlpatterns += [
     url(r'^_cms/', include('uweb.urls'))
 ]
