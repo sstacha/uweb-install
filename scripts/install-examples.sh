015a6cc012730e1f51eba4e7977870e827db44ba
sstacha <sstacha@gmail.com>
2018-10-01 14:28:31 -0500

    Directory copy fix; should now create examples without the directory :)

--- scripts/install-examples.sh
+++ scripts/install-examples.sh
@@ -8,7 +8,7 @@ else
     echo "django project directory [website] found; continuing installation..."
     # make sure we have a example_files to pull from and a files directory to copy them to
     if [[ -d '../example_files/files/' && -d '../website/docroot/files/' ]]; then
-        cp -Rf ../example_files/files/   ../website/docroot/files/
+        cp -Rf ../example_files/files/*   ../website/docroot/files/
     else
         echo 'WARNING: files folder or example_files folder doesnt exist; recommend re-installing...'
     fi
