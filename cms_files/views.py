73ba18d178dfd69c7c11ec3392a0203f2b9967a9
Stephen Stacha <sstacha@gmail.com>
2019-10-04 21:31:22 -0500

    adding views.py changes made to web project back to install

--- cms_files/views.py
+++ cms_files/views.py
@@ -1,25 +1,28 @@
-# NOTE: DO NOT UPDATE THIS FILE
-#   It will be over-written during upgrades
+# NOTE: DO NOT UPDATE THIS FILE - Create a new views_common.py and place any views there if you need it.  then reference
+#   your new one in the urls.py file (which should be edited by you)
 
 import os
 import logging
 import mimetypes
 import importlib.util
 import json
+import codecs
 
 from django.conf import settings
-from django.template import RequestContext, Template, Origin
+# from django.template import Context
+from django.template import Template, Origin, RequestContext
 from django.http import HttpResponse, FileResponse, JsonResponse, HttpResponseForbidden
 from django.views.generic import View
 from django.views.decorators.csrf import csrf_protect
 from django.core import serializers
-from pprint import pprint
+# from pprint import pprint
 # from django.shortcuts import redirect
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from .models import Content
 
 log = logging.getLogger("cms.views")
+
 
 # This view is called from DocrootFallbackMiddleware.process_response
 # when a 404 is raised and we are not working with a template (we want to look for a static file in the docroot)
@@ -52,6 +55,7 @@ def static(request):
     else:
         return None
 
+
 # This view is called from DocrootFallbackMiddleware.process_response
 # when a 404 is raised, which often means CsrfViewMiddleware.process_view
 # has not been called even if CsrfViewMiddleware is installed. So we need
@@ -61,65 +65,178 @@ def static(request):
 # without any CSRF checks. Therefore, we only
 # CSRF protect the internal implementation (render page).
 def page(request):
+    # NEW VERSION: pushed meta processing down to class TemplateMeta
+    meta = TemplateMeta(request)
+    # NOTE: only calls render_page if the template is found
+    return meta.render()
+
+    # """
+    # We are going to try to look for a template a couple of ways based on the request; if nothing is found we return
+    # None
+    #  and things proceed like never called.  If found we return the response it should return instead
+    # """
+    # docroot_dir = getattr(settings, "DOCROOT_ROOT", "")
+    # log.debug("docroot dir: " + docroot_dir)
+    # path = request.path_info
+    # if path.startswith("/"):
+    #     path = path[1:]
+    # file=os.path.join(docroot_dir, path)
+    # log.debug("file: " + file)
+    # url = file
+    # module_name = path
+    # template_name = path
+    # template = None
+    # # if the url ends in .html then try to load a corresponding template from the docroot/files directory
+    # if url.endswith(".html"):
+    #     # our url will request .html but we want to look for a .dt file (required for template processing)
+    #     url = url[:-4]
+    #     url += "dt"
+    #     template_name = template_name[:-4]
+    #     template_name += "dt"
+    #     if os.path.isfile(url):
+    #         log.debug("found file: " + url)
+    #     else:
+    #         url = None
+    #
+    # elif url.endswith('/'):
+    #     url += "index.dt"
+    #     if os.path.isfile(url):
+    #         log.debug("found file: " + url)
+    #         module_name += "index.html"
+    #         template_name += "index.dt"
+    #     else:
+    #         url = None
+    #
+    # else:
+    #     url += ".dt"
+    #     if os.path.isfile(url):
+    #         log.debug("found file: " + url)
+    #         module_name += ".html"
+    #         template_name += ".dt"
+    #     else:
+    #         url = None
+    #
+    # if url:
+    #     log.debug("opening file: " + url)
+    #     # fp = open(url)
+    #     fp = codecs.open(url, "r", encoding='utf-8')
+    #     log.debug("loading template...")
+    #     # template = Template(fp.read(), Origin(url), template_name)
+    #     template = Template(fp.read().encode('utf-8'), Origin(url), template_name)
+    #     log.debug("closing file")
+    #     fp.close()
+    #
+    # if template:
+    #     log.debug("attempting to load context and render the template...")
+    #     return render_page(request, template, module_name)
+    # else:
+    #     return None
+
+
+class TemplateMeta:
     """
-    We are going to try to look for a template a couple of ways based on the request; if nothing is found we return None
-     and things proceed like never called.  If found we return the response it should return instead
+        encapsulates the core atts and methods to get a valid template
     """
-    # for perfonmance reasons and because we want to have our webserver
-    base=getattr(settings, "BASE_DIR", "")
-    log.debug("base: " + base)
-    path = request.path_info
-    if path.startswith("/"):
-        path = path[1:]
-    file=os.path.join(base, "docroot/files", path)
-    log.debug("file: " + file)
-    url = file
-    module_name = path
-    template_name = path
-    template = None
-    # if the url ends in .html then try to load a corresponding template from the docroot/files directory
-    if url.endswith(".html"):
-        # our url will request .html but we want to look for a .dt file (required for template processing)
-        url = url[:-4]
-        url += "dt"
-        template_name = template_name[:-4]
-        template_name += "dt"
-        if os.path.isfile(url):
-            log.debug("found file: " + url)
+
+    def __init__(self, request):
+        # setup our basic attributes for the meta-data we will use for validation and template creation
+        self.is_found = False
+        self.request = request
+        self.docroot_dir = getattr(settings, "DOCROOT_ROOT", "")
+        log.debug("docroot dir: " + self.docroot_dir)
+        self.original_path = request.path_info.strip()
+        self.path = self.original_path
+        if self.path.startswith("/"):
+            self.path = self.path[1:]
+        self.file_name = os.path.join(self.docroot_dir, self.path)
+        log.debug("file: " + str(self.file_name))
+        self.module_name = self.path
+        self.template_name = self.path
+        # try and modify urls for logic on how to pull the correct template
+        self.find_template()
+
+        if not settings.IGNORE_LANGUAGE_PREFIX:
+            # if not found lets try and make sure it is not because of language
+            if not self.is_found and request.LANGUAGE_CODE:
+                # new code to make us django language aware (strip language code when looking for a template)
+                # print('request lang: ' + str(request.LANGUAGE_CODE))
+                # print('settings lang: ' + str(settings.LANGUAGE_CODE))
+                # print('languages: ' + str(settings.LANGUAGES))
+                lang = '/' + request.LANGUAGE_CODE + "/"
+                if self.original_path.startswith(lang):
+                    self.path = self.original_path[len(lang):]
+                    self.file_name = os.path.join(self.docroot_dir, self.path)
+                    log.debug("language stripped file: " + str(self.file_name))
+                    self.module_name = self.path
+                    self.template_name = self.path
+                    # re-try and modify urls for logic on how to pull the correct template
+                    self.find_template()
+
+            # finally if we still don't have a template and ends with / and APPEND_SLASH is set and False strip it
+            if not self.is_found and request.LANGUAGE_CODE and settings.APPEND_SLASH and self.original_path.endswith('/'):
+                lang = '/' + request.LANGUAGE_CODE + "/"
+                if self.original_path.startswith(lang):
+                    # same as above except now try to strip the last slash
+                    self.path = self.original_path[len(lang):len(self.original_path)-1]
+                    self.file_name = os.path.join(self.docroot_dir, self.path)
+                    log.debug("language stripped file: " + str(self.file_name))
+                    self.module_name = self.path
+                    self.template_name = self.path
+
+                    # re-try and modify urls for logic on how to pull the correct template
+                    self.find_template()
+
+
+    # contains the logic for taking a request url and attempting to locate a page template for it
+    def find_template(self):
+        # if the url ends in .html then try to load a corresponding template from the docroot/files directory
+        if self.file_name.endswith(".html"):
+            # our url will request .html but we want to look for a .dt file (required for template processing)
+            self.file_name = self.file_name[:-4]
+            self.file_name += "dt"
+            self.template_name = self.template_name[:-4]
+            self.template_name += "dt"
+            if os.path.isfile(self.file_name):
+                log.debug("found file: " + self.file_name)
+                self.is_found = True
+
+        elif self.file_name.endswith('/'):
+            self.file_name += "index.dt"
+            if os.path.isfile(self.file_name):
+                log.debug("found file: " + self.file_name)
+                self.module_name += "index.html"
+                self.template_name += "index.dt"
+                self.is_found = True
+
         else:
-            url = None
+            self.file_name += ".dt"
+            if os.path.isfile(self.file_name):
+                log.debug("found file: " + self.file_name)
+                self.module_name += ".html"
+                self.template_name += ".dt"
+                self.is_found = True
 
-    elif url.endswith('/'):
-        url += "index.dt"
-        if os.path.isfile(url):
-            log.debug("found file: " + url)
-            module_name += "index.html"
-            template_name += "index.dt"
-        else:
-            url = None
+    def render(self):
+        if self.is_found:
+            log.debug("opening file: " + self.file_name)
+            fp = codecs.open(self.file_name, "r", encoding='utf-8')
+            log.debug("loading template...")
+            # template = Template(fp.read(), Origin(url), template_name)
+            template = Template(fp.read().encode('utf-8'), Origin(self.file_name), self.template_name)
+            log.debug("closing file")
+            fp.close()
 
-    else:
-        url += ".dt"
-        if os.path.isfile(url):
-            log.debug("found file: " + url)
-            module_name += ".html"
-            template_name += ".dt"
-        else:
-            url = None
+            if template:
+                log.debug("attempting to load context and render the template...")
+                return render_page(self.request, template, self.module_name)
+            else:
+                return None
 
-    if url:
-        log.debug("opening file: " + url)
-        fp = open(url)
-        log.debug("loading template...")
-        template = Template(fp.read(), Origin(url), template_name)
-        log.debug("closing file")
-        fp.close()
+    def is_found(self):
+        return self.is_found
 
-    if template:
-        log.debug("attempting to load context and render the template...")
-        return render_page(request, template, module_name)
-    else:
-        return None
+    def __str__(self):
+        return self.file_name
 
 
 @csrf_protect
@@ -149,6 +266,7 @@ def render_page(request, template, module_name):
         # datafile = imp.load_source(module_name, datafile_name)
         # note changing datafile below to data
     except Exception:
+        # logging.error(traceback.format_exc())
         data = None
     if data:
         try:
@@ -162,11 +280,13 @@ def render_page(request, template, module_name):
                 context = getattr(data, 'context')
             except AttributeError:
                 context = {}
-    log.debug("context string: " + str(context))
+    # print("context string: " + str(context))
     template_context = RequestContext(request)
-    template_context.push(context)
+    if (context):
+        template_context.push(context)
     response = HttpResponse(template.render(template_context))
     return response
+
 
 # class based view for getting and putting cms content
 class ContentApi(View):
@@ -235,7 +355,8 @@ class ContentApi(View):
 #                 if r.status_code >= 200 and r.status_code < 400:
 #                     check_status = r.status_code
 #                     # if we were intercepted by a login screen then we treat that as a forbidden instead of success
-#                     if r.request.path_url.lower().startswith('/en/admin/login/' or r.request.path_url.lower().startswith('/admin/login/')):
+#                     if r.request.path_url.lower().startswith('/en/admin/login/' or r.request.path_url.lower(
+#                     ).startswith('/admin/login/')):
 #                         # print('starts with login')
 #                         check_response = False
 #                         is_valid = False
